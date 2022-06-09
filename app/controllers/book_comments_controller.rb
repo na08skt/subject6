732364251a7ed1356bacf493050e9465825364a5
na08skt <sakata.na08@gmail.com>
2022-06-09 13:29:12 +0000

    [FinishCmmit]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,21 +1,21 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     # したがいつもの記述
     # comment = BookComment.new(book_comment_params)
     # comment.user_id = current_user.id
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book.id)
+    @comments = @book.book_comments
   end
 
   def destroy
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(book)
+    @comments = @book.book_comments
   end
+
 
   private
   def book_comment_params
